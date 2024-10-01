c5436ec54726cab7303b71e0655f8ea2cc0afd0c
José Cardoso <ddeviljo@gmail.com>
2024-09-30 23:28:14 -0300

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -127,7 +127,7 @@ server {
     add_header Alt-Svc  'h2=":$server_port"; ma=2592000;';
 
     location ~ ^\/api\/servers\/(?<serverid>.*)?\/ws$ {
-        proxy_pass http://127.0.0.1:8443/api/servers/\$serverid/ws;
+        proxy_pass https://127.0.0.1:8443/api/servers/\$serverid/ws;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
@@ -142,7 +142,7 @@ server {
     }
 
     location / {
-        proxy_pass http://127.0.0.1:8443/;
+        proxy_pass https://127.0.0.1:8443/;
         proxy_set_header Host \$host;
         client_max_body_size 50m;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
